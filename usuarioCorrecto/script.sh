ef7a7ba44c1498504bbbaa7c81a3454e7da4ae90
Pablo223 <37366362+Pablo223@users.noreply.github.com>
2020-05-14 22:23:40 +0200

    En caso de ser incorrectos usuario/contraseña

---
+++ usuarioCorrecto/script.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+read -p 'Usuario : ' user
+read -sp 'Contraseña : ' pass
+
+if [[$user != "admin" && $pass != "123" ]]
+
+echo "Nombre usuario y contraseña incorrectos"
+
+mysl(){
+chmod 000 clave1.txt
+}
+
+fi
+
+ 
